e74cbefb731c4d176fb240d141005db12805076a
Rakeshdarda19 <rakeshdarda19@gmail.com>
2022-09-20 22:51:09 +0530

    added field in post method

--- src/routes/asset.js
+++ src/routes/asset.js
@@ -39,6 +39,45 @@ router.post('/', async (req, res) => {
     try {
         const asset = await assetModel.create({
             //mention as per the schema which is to be created
+            propertyType : req.body.propertyType,
+            negotable : req.body.negotable,
+            price : req.body.price,
+            ownership : req.body.ownership,
+            propertyAge : req.body.propertyAge,
+            propertyApproved : req.body.propertyApproved,
+            propertyDescription : req.body.propertyDescription,
+            bankLoan : req.body.bankLoan,
+
+            length : req.body.length,
+            breath : req.body.breath,
+            totalArea : req.body.totalArea,
+            areaUnit : req.body.areaUnit,
+            noOfBHK : req.body.noOfBHK,
+            noOfFloor : req.body.noOfFloor,
+            attached : req.body.attached,
+            western : req.body.western,
+            furnished : req.body.furnished,
+            carParking : req.body.carParking,
+            lift : req.body.lift,
+            electricity : req.body.electricity,
+            facing : req.body.facing,
+
+            name : req.body.name,
+            mobile : req.body.mobile,
+            postedBy : req.body.postedBy,
+            saleType : req.body.saleType,
+            featuredPackage : req.body.featuredPackage,
+            ppdPackage : req.body.ppdPackage,
+
+            email : req.body.email,
+            city : req.body.city,
+            area : req.body.area,
+            pincode : req.body.pincode,
+            address : req.body.address,
+            landmark : req.body.landmark,
+            latitude : req.body.latitude,
+            longitude : req.body.longitude
+
         })
         res.status(200).json({
             message: "success",
